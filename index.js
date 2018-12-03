21862a40587c357d52b0ff67c2508093950038d2
Erick Menezes <erickbmenezes@gmail.com>
2018-12-03 21:33:37 -0200

    refactorando a preparação dos nós para melhor entendimento

--- index.js
+++ index.js
@@ -2,29 +2,8 @@
 
 const fs = require('fs');
 
-let nodes = [];
-
 let graph = JSON.parse(fs.readFileSync('./grafo.json', 'utf8'));
 let graphLinks = graph.links;
-
-graph.nodes.forEach(function(node) {
-  let data = {
-    id: node.id,
-    visited: false
-  };
-
-  let nodeLinks = [];
-  node.ports.forEach(function(port) {
-    port.links.forEach(function(link) {
-      let child = graphLinks.find(function(l) { return l.id === link; });
-
-      nodeLinks.push(child.target);
-    });
-  });
-  data.links = nodeLinks;
-
-  nodes.push(data);
-});
 
 function bfs(_nodes) {
   let firstNode = _nodes[0]; 
@@ -49,4 +28,29 @@ function bfs(_nodes) {
   }
 }
 
-bfs(nodes);
+function getNodes() {
+  let nodes = [];
+
+  graph.nodes.forEach(function(node) {
+    let data = {
+      id: node.id,
+      visited: false
+    };
+    
+    let nodeLinks = [];
+    node.ports.forEach(function(port) {
+      port.links.forEach(function(link) {
+        let child = graphLinks.find(function(l) { return l.id === link; });
+        
+        nodeLinks.push(child.target);
+      });
+    });
+    data.links = nodeLinks;
+    
+    nodes.push(data);
+  });
+
+  return nodes;
+}
+
+bfs(getNodes());
